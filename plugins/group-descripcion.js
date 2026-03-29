3984ed81309f1ed8b2b440eac1a43152cf270947
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-03-29 00:51:31 -0500

    Update group-descripcion.js

--- plugins/group-descripcion.js
+++ plugins/group-descripcion.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*🔪 𝘓𝘢 𝘥𝘦𝘴𝘤𝘳𝘪𝘱𝘤𝘪ó𝘯 𝘥𝘦𝘭 𝘨𝘳𝘶𝘱𝘰 𝘴𝘦 𝘮𝘰𝘥𝘪𝘧𝘪𝘤𝘰 𝘤𝘰𝘳𝘳𝘦𝘤𝘵𝘢𝘮𝘦𝘯𝘵𝘦*');
+  m.reply('> ➤『 𝘓𝘢 𝘥𝘦𝘴𝘤𝘳𝘪𝘱𝘤𝘪ó𝘯 𝘥𝘦𝘭 𝘨𝘳𝘶𝘱𝘰 𝘴𝘦 𝘮𝘰𝘥𝘪𝘧𝘪𝘤𝘰 𝘤𝘰𝘳𝘳𝘦𝘤𝘵𝘢𝘮𝘦𝘯𝘵𝘦 🌟』');
 };
 handler.help = ['groupdesc <text>'];
 handler.tags = ['grupo'];
