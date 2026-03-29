43ffa408cd1f1f47f9b5a5ddf4e8aa6ac2541dec
𝘑𝘶𝘢𝘯𝘊𝘰𝘳𝘦𝘞𝘻 <jbolnos85@gmail.com>
2026-03-29 00:50:16 -0500

    Update demotion messages in group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -13,12 +13,12 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
     }
 
     if (!user) {
-        return conn.reply(m.chat, `*🥷🏻 𝗠𝗘𝗡𝗖𝗜𝗢𝗡𝗔 𝗔 𝗨𝗡𝗔 𝗣𝗘𝗥𝗦𝗢𝗡𝗔 𝗤𝗨𝗘 𝗗𝗘𝗦𝗘𝗔𝗦 𝗗𝗘𝗚𝗥𝗔𝗗𝗔𝗥*`, m, rcanal);
+        return conn.reply(m.chat, `> ➤『 𝗠𝗘𝗡𝗖𝗜𝗢𝗡𝗔 𝗔 𝗨𝗡𝗔 𝗣𝗘𝗥𝗦𝗢𝗡𝗔 𝗤𝗨𝗘 𝗗𝗘𝗦𝗘𝗔𝗦 𝗗𝗘𝗚𝗥𝗔𝗗𝗔𝗥 🍭 』`, m, rcanal);
     }
 
     try {
         await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
-        await conn.reply(m.chat, `🔪  𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗚𝗥𝗔𝗗𝗔𝗗𝗢 𝗖𝗢𝗡 𝗘𝗫𝗜𝗧𝗢`, m, rcanal);
+        await conn.reply(m.chat, `> ➤『 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗚𝗥𝗔𝗗𝗔𝗗𝗢 𝗖𝗢𝗡 𝗘𝗫𝗜𝗧𝗢 🍭 』`, m, rcanal);
     } catch (e) {
         console.error(e);
         await conn.reply(m.chat, `🚫 Ha ocurrido un error al intentar degradar al usuario.`, m, rcanal);
